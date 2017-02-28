42f538309220914a9fce47b4dafd9b7d224708bf
iibeeing <316553858@qq.com>
2017-02-28 17:10:01 +0800

    1、更换项目目录到OneDrive中；

--- SpringMango/src/main/java/com/mango/jtt/core/base/SysProviderImpl.java
+++ SpringMango/src/main/java/com/mango/jtt/core/base/SysProviderImpl.java
@@ -1,10 +1,8 @@
 package com.mango.jtt.core.base;
-
+// 注意这里的Service类，如果是非分布式，不使用dubbo则用Spring的类，否则使用dubbo的类
 //import com.alibaba.dubbo.config.annotation.Service;
 import org.springframework.stereotype.Service;
-
 import com.mango.jtt.provider.ISysProvider;
-
 //@Service(interfaceClass = ISysProvider.class)
 @Service
 public class SysProviderImpl extends BaseProviderImpl implements ISysProvider {
